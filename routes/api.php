7d08d528b8272d8e96315fc5fad800c28bbd58c0
kawogo <dkawogo79@gmail.com>
2023-12-15 09:34:41 +0300

    WIP:Auth config

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,9 @@
 <?php
 
-use App\Http\Controllers\Api\JobController;
-use App\Http\Controllers\api\RequirementController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Laravel\Fortify\Http\Controllers\AuthenticatedSessionController;
+use Laravel\Fortify\RoutePath;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,10 +15,19 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+$limiter = config('fortify.limiters.login');
+
+Route::post(RoutePath::for('login', '/login'), [AuthenticatedSessionController::class, 'store'])
+->middleware(array_filter([
+    'guest:'.config('fortify.guard'),
+    $limiter ? 'throttle:'.$limiter : null,
+]));
+
+Route::post(RoutePath::for('logout', '/logout'), [AuthenticatedSessionController::class, 'destroy'])
+->name('logout');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('jobs', JobController::class);
-Route::apiResource('jobs.requirements', Requirementcontroller::class)->scoped(['job' => 'job_id']);
+// Route::apiResource('jobs', [])
